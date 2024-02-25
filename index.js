8c487b29a82dd29418a17fcde76eafdd8a2deea4
Dalton Riehl <dtonriehl@gmail.com>
2024-02-25 00:54:17 -0600

    Array Filter Method Lab Complete: Achievement Get!

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching (driverArray, name) {
+    return driverArray.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch (driverArray, letters) {
+    return driverArray.filter(function(driver) {
+        return driver.startsWith(letters);
+    });
+}
+
+function matchName (driverObjects, string) {
+    return driverObjects.filter(function (element) {
+        return element.name === string;
+    });
+}
